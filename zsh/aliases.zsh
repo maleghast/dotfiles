90689fe03367e00904a0a12d6f1aa2481c0970fb
Simon Thulbourn <simon+github@thulbourn.com>
2012-08-30 10:22:34 +0100

    adds new aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -24,4 +24,10 @@ function fcl() {
   curl --cacert /Users/sth/.certs/ca.pem --cert /Users/sth/.certs/dev.bbc.co.uk.pem $@
 }
 
+function candy() {
+  curl --cacert /Users/sth/.certs/ca.pem --cert /Users/sth/.certs/dev.bbc.co.uk.pem "https://api.$1.bbc.co.uk/content/asset/news/$2" -i -H 'X-Candy-Platform: EnhancedMobile' -H 'X-Candy-Audience: domestic' -H 'Accept: application/json'
+}
+
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
+alias tmux='TERM=screen-256color-bce tmux'
