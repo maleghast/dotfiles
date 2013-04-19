ffce3136d36221c63de65d1b225eba48695a196c
Simon Thulbourn <simon+github@thulbourn.com>
2013-04-19 09:10:17 +0100

    adds a few new aliases

--- zsh/conf.d/aliases.zsh
+++ zsh/conf.d/aliases.zsh
@@ -18,3 +18,25 @@ function candy() {
 }
 
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en1"
+alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
+
+# Enhanced WHOIS lookups
+alias whois="whois -h whois-servers.net"
+
+# Flush Directory Service cache
+alias flush="dscacheutil -flushcache"
+
+# View HTTP traffic
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+# One of @janmoesen’s ProTip™s
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+	alias "$method"="lwp-request -U -e -m '$method'"
+done
+
+alias hosts="sudo $EDITOR /etc/hosts"
