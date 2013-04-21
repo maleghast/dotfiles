8cb8c316f1b7d37c0b12414db47a0bb915311bdf
Simon Thulbourn <simon+github@thulbourn.com>
2013-04-21 19:17:05 +0100

    changes fcl func

--- zsh/conf.d/aliases.zsh
+++ zsh/conf.d/aliases.zsh
@@ -10,11 +10,7 @@ function md5() {
 }
 
 function fcl() {
-  curl --cacert /Users/sth/.certs/ca.pem --cert /Users/sth/.certs/dev.bbc.co.uk.pem $@
-}
-
-function candy() {
-  curl --cacert /Users/sth/.certs/ca.pem --cert /Users/sth/.certs/dev.bbc.co.uk.pem "https://api.$1.bbc.co.uk/content/asset/news/$2" -i -H 'X-Candy-Platform: EnhancedMobile' -H 'X-Candy-Audience: domestic' -H 'Accept: application/json'
+  curl --cacert $DEV_CA_PATH --cert $DEV_CERT_PATH $@
 }
 
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
