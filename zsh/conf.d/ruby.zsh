a3e0281cd75a1cff4c3e5acb3bc995669bb7e711
Simon Thulbourn <simon+github@thulbourn.com>
2013-05-26 23:16:01 +0100

    changes path
    TODO: change so it's path independant

--- zsh/conf.d/ruby.zsh
+++ zsh/conf.d/ruby.zsh
@@ -1,5 +1,5 @@
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
+source /opt/boxen/homebrew/opt/chruby/share/chruby/chruby.sh
+source /opt/boxen/homebrew/opt/chruby/share/chruby/auto.sh
 
 function rb() {
   ruby-build $1 /opt/rubies/$1
