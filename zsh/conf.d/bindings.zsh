6ccb97ea1f1265bac39600e17b234f90e305825d
Simon Thulbourn <simon+github@thulbourn.com>
2013-06-19 11:19:09 +0100

    updates bindings

--- zsh/conf.d/bindings.zsh
+++ zsh/conf.d/bindings.zsh
@@ -30,8 +30,25 @@ set completion-ignore-case on
 # set completion-prefix-display-length 2
 
 
-bindkey -v
-bindkey '\e[3~' delete-char
-bindkey '^R' history-incremental-search-backward
+# bindkey -v
+# bindkey '\e[3~' delete-char
+# bindkey '^R' history-incremental-search-backward
 
+bindkey -e
+bindkey '\ew' kill-region
+bindkey -s '\el' "ls\n"
+bindkey '^r' history-incremental-search-backward
+bindkey "^[[5~" up-line-or-history
+bindkey "^[[6~" down-line-or-history
 
+# make search up and down work, so partially type and hit up/down to find relevant stuff
+bindkey '^[[A' up-line-or-search
+bindkey '^[[B' down-line-or-search
+
+bindkey ' ' magic-space
+
+# Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
+bindkey '^?' backward-delete-char
+bindkey "^[[3~" delete-char
+bindkey "^[3;5~" delete-char
+bindkey "\e[3~" delete-char
