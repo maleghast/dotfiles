8c1a6dd7b53a310ccecd080f87eab64b51086613
Simon Thulbourn <simon+github@thulbourn.com>
2013-06-19 11:19:36 +0100

    updates completion

---
+++ zsh/conf.d/completion.zsh
@@ -0,0 +1,9 @@
+# do not autoselect the first completion entry
+unsetopt menu_complete
+unsetopt flowcontrol
+setopt auto_menu
+
+# show completion menu on succesive tab press
+setopt complete_in_word
+setopt always_to_end
+
